843452a9c4682cb7483e2369d1c9fd1dd9e59be9
mxk624 <mxk624@gmail.com>
2024-08-01 11:36:38 +0900

    デプロイ

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Entry point for the build script in your package.json
-import "bootstrap"
-import "@popperjs/core"
-import "../stylesheets/application"
 import "@hotwired/turbo-rails"
 import "./controllers"
